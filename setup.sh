2b51cf2c659e819bd2177452d25553f12580dd61
Dolores Miao <wjmiao@ucdavis.edu>
2024-09-26 14:20:38 -0700

    PolyBench patch

--- setup.sh
+++ setup.sh
@@ -4,3 +4,12 @@ git clone https://github.com/GMAP/NPB-GPU
 cd NPB-GPU
 git checkout f65e28b349bd08594bf94fa22c090c4d70e90ccb
 git apply ../NPB-GPU-patch.txt
+
+# PolyBench-ACC
+cd ~/hipec/benchmarks
+wget https://github.com/cavazos-lab/PolyBench-ACC/archive/refs/tags/v0.1.zip
+unzip v0.1.zip
+cd PolyBench-ACC-0.1
+rm -r HMPP/ OpenACC/ OpenCL/ OpenMP/
+cd ..
+patch -p0 < PolyBench-patch.txt
