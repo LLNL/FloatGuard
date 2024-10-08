332688442f59d57e0a84968fb6e78ca114e4f95a
Dolores Miao <wjmiao@ucdavis.edu>
2024-10-07 17:50:40 -0700

    software version in docker container docker tag in run script

--- build_llvm_env.sh
+++ build_llvm_env.sh
@@ -2,14 +2,13 @@ apt-get update -y
 apt-get install -y apt-utils
 DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
 apt-get upgrade -y
-apt-get install -y python3 python3-pip \
-                    git \
-                    wget \
-                    build-essential \
-                    cmake \
-                    libboost-all-dev \
-                    lsb-release \
-                    software-properties-common \
-                    rocm-llvm-dev
+apt-get install -y python3=3.10.6-1~22.04.1 python3-pip=22.0.2+dfsg-1ubuntu0.4 \
+                    git=1:2.34.1-1ubuntu1.11 \
+                    wget=1.21.2-2ubuntu1.1 \
+                    build-essential=12.9ubuntu3 \
+                    cmake=3.22.1-1ubuntu1.22.04.2 \
+                    lsb-release=11.1.0ubuntu4 \
+                    software-properties-common=0.99.22.9 \
+                    rocm-llvm-dev=17.0.0.24193.60102-119~22.04
 
-pip3 install pexpect
+pip3 install pexpect==4.9.0
--- run_docker.sh
+++ run_docker.sh
@@ -3,5 +3,5 @@ if [ $? == 0 ]; then
     docker start hipec
     docker exec -it --user root hipec bash
 else
-    docker run -it -v "$PWD":/root/hipec --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --name hipec doloresmiao/hipec:latest
+    docker run -it -v "$PWD":/root/hipec --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --name hipec ucdavisplse/hipec:latest
 fi
