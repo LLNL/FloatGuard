eabef1db183087ae30cd36c282851c89cc7d90ed
Dolores Miao <wjmiao@ucdavis.edu>
2024-10-09 09:52:08 -0700

    remove absolute dir requirement for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 # NPB-GPU
-cd ~/FloatGuard/benchmarks
+cd benchmarks
 git clone https://github.com/GMAP/NPB-GPU
 cd NPB-GPU
 git checkout f65e28b349bd08594bf94fa22c090c4d70e90ccb
 git apply ../NPB-GPU-patch.txt
+cd ..
 
 # PolyBench-ACC
-cd ~/FloatGuard/benchmarks
 wget https://github.com/cavazos-lab/PolyBench-ACC/archive/refs/tags/v0.1.zip
 unzip v0.1.zip
 rm v0.1.zip
@@ -17,7 +17,6 @@ patch -p0 < PolyBench-patch.txt
 
 # Rodinia
 
-cd ~/FloatGuard/benchmarks
-source setup_rodinia.sh
+sh setup_rodinia.sh
 
-cd ~/FloatGuard
+cd ..
