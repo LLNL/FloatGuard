08129cb490cc29e9c775d12abd446bea84ce5eff
Dolores Miao <miao2@llnl.gov>
2024-10-22 14:57:06 -0700

    remove read command from setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ git checkout f65e28b349bd08594bf94fa22c090c4d70e90ccb
 git apply ../NPB-GPU-patch.txt
 cd ..
 
-read -p "Press enter to continue"
-
 # PolyBench-ACC
 wget https://github.com/cavazos-lab/PolyBench-ACC/archive/refs/tags/v0.1.zip
 unzip v0.1.zip
@@ -16,8 +14,6 @@ cd PolyBench-ACC-0.1
 rm -r HMPP/ OpenACC/ OpenCL/ OpenMP/
 cd ..
 patch -p0 < PolyBench-patch.txt
-
-read -p "Press enter to continue"
 
 # Rodinia
 
