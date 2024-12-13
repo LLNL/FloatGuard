c204eb60859010cdd47d3ea4605875a89bdcc422
Dolores Miao <wjmiao@ucdavis.edu>
2024-12-13 14:56:55 -0800

    fix link error

--- gdb_script/hipcc_wrapper.py
+++ gdb_script/hipcc_wrapper.py
@@ -17,18 +17,23 @@ if __name__ == "__main__":
         if arg == "--hip-link":
             link_time = True
 
-    disable_all = True
+    disable_all = False
     replaced_argv = ["hipcc"]
     for arg in argv[1:]:
-        if not disable_all and arg.endswith(".o") and not "InstStub.o" in arg and os.path.exists(arg):
+        if not disable_all and arg.endswith(".o") and not "InstStub.o" in arg:
             arg_s = arg[:-2] + ".s"
             if not link_time:
-                replaced_argv.append(arg[:-2] + ".s") 
-            elif os.path.exists(arg):
-                replaced_argv.append(arg[:-2] + ".s")  
+                replaced_argv.append(arg_s) 
+            elif os.path.exists(arg_s):
+                replaced_argv.append(arg_s)  
             else:
                 replaced_argv.append(arg)
         else:
             replaced_argv.append(arg)
+
+    if not disable_all and not link_time:
+        replaced_argv.append("-S")
+
+    print(replaced_argv)
 
     subprocess.run(replaced_argv)
