77d15f01d9c4a851cfaf422d6a9d860035cfa618
Dolores Miao <wjmiao@ucdavis.edu>
2025-01-17 12:37:16 -0800

    move InstStub.o reference to the wrapper script

--- benchmarks/PolyBench-ACC-0.1/CUDA/utilities/common.mk
+++ benchmarks/PolyBench-ACC-0.1/CUDA/utilities/common.mk
@@ -15,11 +15,11 @@ endif
 FP_FLAGS = -DDATA_TYPE=double -DDATA_PRINTF_MODIFIER=\"%0.2lf\"
 
 all:
-	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh ${HOME}/FloatGuard/inst_pass/Inst/InstStub.o -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_mini ${INST_FLAGS} ${FP_FLAGS} -DMINI_DATASET
-	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh ${HOME}/FloatGuard/inst_pass/Inst/InstStub.o -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_small ${INST_FLAGS} ${FP_FLAGS} -DSMALL_DATASET
-	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh ${HOME}/FloatGuard/inst_pass/Inst/InstStub.o -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_standard ${INST_FLAGS} ${FP_FLAGS} -DSTANDARD_DATASET
-	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh ${HOME}/FloatGuard/inst_pass/Inst/InstStub.o -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_large ${INST_FLAGS} ${FP_FLAGS} -DLARGE_DATASET
-	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh ${HOME}/FloatGuard/inst_pass/Inst/InstStub.o -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_extralarge ${INST_FLAGS} ${FP_FLAGS} -DEXTRALARGE_DATASET
+	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh  -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_mini ${INST_FLAGS} ${FP_FLAGS} -DMINI_DATASET
+	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh  -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_small ${INST_FLAGS} ${FP_FLAGS} -DSMALL_DATASET
+	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh  -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_standard ${INST_FLAGS} ${FP_FLAGS} -DSTANDARD_DATASET
+	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh  -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_large ${INST_FLAGS} ${FP_FLAGS} -DLARGE_DATASET
+	${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh  -O3 -ffast-math ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE}_extralarge ${INST_FLAGS} ${FP_FLAGS} -DEXTRALARGE_DATASET
 	
 clean:
 	rm -f *~ *.s *.exe* seq.txt 
