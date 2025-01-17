6a3b57e88d92e0d7de1fe2ce49f2357da220b31e
Dolores Miao <wjmiao@ucdavis.edu>
2025-01-17 13:56:18 -0800

    GPGPU_SIM wrapper

--- benchmarks/GPGPU_SIM/shared.mk
+++ benchmarks/GPGPU_SIM/shared.mk
@@ -4,9 +4,9 @@ CC_FLAGS = -Wall
 #CC_FLAGS = -O3 
 
 # CUDA compiler
-NVCC = hipcc #${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh
+NVCC = ${HOME}/FloatGuard/gdb_script/hipcc_wrapper.sh
 #NVCC_FLAGS = -arch=sm_70  #-O3 
 NVCC_FLAGS = -g -O3 -I. -I..
 # Link
 
-LINK_FLAG = -fgpu-rdc --hip-link #${HOME}/FloatGuard/inst_pass/Inst/InstStub.o
+LINK_FLAG = -fgpu-rdc --hip-link #
