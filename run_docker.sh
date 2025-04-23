b566d0c35555f4e76f4d4cd0c32ca6a4132b9b08
Dolores Miao <wjmiao@ucdavis.edu>
2025-04-23 12:43:28 -0700

    remove Docker related files

--- run_docker.sh
+++
@@ -1,7 +0,0 @@
-docker container ls -a | grep 'FloatGuard' &> /dev/null
-if [ $? == 0 ]; then
-    docker start FloatGuard
-    docker exec -it --user root FloatGuard bash
-else
-    docker run -it -v "$PWD":/root/FloatGuard --device /dev/kfd --device /dev/dri/renderD128 --security-opt seccomp=unconfined --name FloatGuard ucdavisplse/floatguard:latest
-fi
